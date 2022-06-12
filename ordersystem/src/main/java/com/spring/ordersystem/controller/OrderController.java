cf09075c1a07db11e462072f482ae82719a62509
ParkSungGyu1 <steven0507@naver.com>
2022-06-12 10:30:01 +0900

    CORS 설정

--- ordersystem/src/main/java/com/spring/ordersystem/controller/OrderController.java
+++ ordersystem/src/main/java/com/spring/ordersystem/controller/OrderController.java
@@ -6,10 +6,7 @@ import com.spring.ordersystem.dto.OrderResponse;
 import com.spring.ordersystem.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
